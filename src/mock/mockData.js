f045b88aba019032850b5721ab668d911dd1abf8
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-07-08 23:52:24 -0600

    Create mockData.js

---
+++ src/mock/mockData.js
@@ -0,0 +1,59 @@
+export const categories = ["Deportiva", "Casual", "Erótica", "Descanso"];
+
+
+export const products = [
+    {
+        id: 1,
+        name: "boxer",
+        description: "Cómodos bóxers de algodón azul perfectos para el uso diario.",
+        price: 100,
+        stock: 10,
+        category: ["Casual"],
+        image: "path/to/boxer-image.jpg"
+    },
+    {
+        id: 2,
+        name: "bralette",
+        description: "Bralette de encaje suave que ofrece soporte y estilo, ideal para el descanso.",
+        price: 150,
+        stock: 15,
+        category: ["Descanso"],
+        image: "path/to/bralette-image.jpg"
+    },
+    {
+        id: 3,
+        name: "tanga",
+        description: "Tanga de microfibra transpirable diseñada para un ajuste cómodo durante el ejercicio.",
+        price: 120,
+        stock: 20,
+        category: ["Deportiva"],
+        image: "path/to/tanga-image.jpg"
+    },
+    {
+        id: 4,
+        name: "jockstrap",
+        description: "Jockstrap de diseño ergonómico, ideal para deportes de alto rendimiento.",
+        price: 130,
+        stock: 25,
+        category: ["Deportiva"],
+        image: "path/to/jockstrap-image.jpg"
+    },
+    {
+        id: 5,
+        name: "lencería",
+        description: "Conjunto de lencería erótica con detalles en encaje, perfecto para ocasiones especiales.",
+        price: 200,
+        stock: 5,
+        category: ["Erótica"],
+        image: "path/to/lenceria-image.jpg"
+    },
+    {
+        id: 6,
+        name: "bikini",
+        description: "Bikini de algodón orgánico para uso casual, disponible en varios colores.",
+        price: 110,
+        stock: 30,
+        category: ["Casual"],
+        image: "path/to/bikini-image.jpg"
+    }
+];
