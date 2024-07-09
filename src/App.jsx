636b72ec6eea3f0c0213aa3ce419a390c780df47
Jorge Roblez <jorgemiguelperezroblez@gmail.com>
2024-07-09 16:30:44 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import Hero from './Page/Hero';
 import Navbar from './components/Navbar/Navbar.jsx';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
 
 function App() {
+  const title = "Titulo ejemplo"; 
+
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Hero />
+      <ItemListContainer title={title} />
     </div>
   );
 }
