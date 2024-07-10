961c44f90d9b0f7f66c3f85e3b2e67b55bd8df9a
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-07-09 23:00:47 -0600

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
-import Navitem from "./Navitem.jsx";
-import { categories } from "../../mock/mockData.js";
-import CartItem from "./Carticon.jsx";
+import Navitem from './Navitem.jsx';
+import { categories } from '../../mock/mockData.js';
+import CartWidget from '../CartWidget/CartWidget.jsx';
 
-const Navbar = (props) => {
+const Navbar = () => {
   return (
     <nav className="fixed top-2 left-0 right-0 bg-slate-100 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-70 border border-slate-200 z-50 mx-auto text-slate-600 p-4 rounded-full max-w-xl shadow-sm">
       <div className="flex justify-center">
@@ -10,10 +10,11 @@ const Navbar = (props) => {
           <Navitem name={categories[0]} />
           <Navitem name={categories[1]} />
           <a href="#" className="hover:scale-105 transform transition">
+            <img src="/images/Logo-webpage.png" alt="Logo website" />
           </a>
           <Navitem name={categories[2]} />
           <Navitem name={categories[3]} />
-          <CartItem />
+          <CartWidget />
         </ul>
       </div>
     </nav>
