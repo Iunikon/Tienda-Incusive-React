2eaf0de8c9b4102b7288c7e18d92f3b73dcb8bf2
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-05 20:12:19 -0600

    Else titulo

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,23 +1,23 @@
 import ItemList from "../Itemlist/ItemList";
 import { products } from "../../mock/mockData";
 
-
 const ItemListContainer = ({ title }) => {
-    let titleToShow
-    if(title != undefined){
-        titleToShow = "No hay productos"
+    let titleToShow; // Declarar `titleToShow`
+
+    if (title !== undefined) {
+        titleToShow = "No hay productos";
+    } else {
+        titleToShow = title; // Asignar `title` a `titleToShow`
     }
 
-    
     return (   
-    <div className="mx-auto container">
-        <div className="">
-            <h2 className="text-3xl font-bold text-slate-700 mb-1">{titleToShow}  </h2> {/* Ejemplo de estilos */}
-            <ItemList products={products} />
+        <div className="mx-auto container">
+            <div className="">
+                <h2 className="text-3xl font-bold text-slate-700 mb-1">{titleToShow}</h2> {/* Ejemplo de estilos */}
+                <ItemList products={products} />
+            </div>
         </div>
-
-    </div>
-);
+    );
 };
 
 export default ItemListContainer;
