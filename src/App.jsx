8228ae8777e04374a42e048af3e290e404a42f2f
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-08 00:39:22 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
 import Layout from './page/Layout.jsx';
-import Main from './Hero/Main.jsx';
+import Main from './Main/Main.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import AboutUs from './components/AboutUs/AbouUs.jsx';
 
