8626df877959261b8c7b4555e7a38595434f7d3a
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-07 23:49:24 -0600

    Contacto y about

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="fixed top-2 left-0 right-0 bg-slate-100 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-70 border border-slate-200 z-50 mx-auto text-slate-600 p-4 rounded-full max-w-xl shadow-sm">
+    <nav className="fixed top-2 left-0 right-0 bg-slate-100 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-70 border border-slate-200 z-50 mx-auto text-slate-600 p-4 rounded-full max-w-3xl shadow-sm">
       <div className="flex justify-center">
         <ul className="flex space-x-2 items-center">
           <Link to="/" className="mx-4 hover:scale-105 transform transition">
@@ -14,6 +14,10 @@ const Navbar = () => {
           {categories.map((category, id) => (
             <Navitem key={id} category={category} />
           ))}
+          <Link 
+            to="/Contact" 
+            className="px-4 py-2.5 text-sm font-semibold bg-transparent rounded-full ml-1 focus:text-white hover:text-white hover:bg-violet-600 focus:bg-violet-600 focus:outline-none focus:shadow-outline">Contacto</Link>
+          <Link to="/AboutUs" className="px-4 py-2.5 text-sm font-semibold bg-transparent rounded-full ml-1 focus:text-white hover:text-white hover:bg-violet-600 focus:bg-violet-600 focus:outline-none focus:shadow-outline">About</Link>
           <CartWidget />
         </ul>
       </div>
