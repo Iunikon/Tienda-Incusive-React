b6d678d1c415d56e95fb509a1d30ae4a60e14633
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-07 23:44:21 -0600

    Mapeo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import Navitem from './Navitem.jsx';
 import { categories } from '../../mock/mockData.js';
 import CartWidget from '../CartWidget/CartWidget.jsx';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="fixed top-2 left-0 right-0 bg-slate-100 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-70 border border-slate-200 z-50 mx-auto text-slate-600 p-4 rounded-full max-w-xl shadow-sm">
       <div className="flex justify-center">
         <ul className="flex space-x-2 items-center">
-          <a href="#" className="mx-4 hover:scale-105 transform transition">
+          <Link to="/" className="mx-4 hover:scale-105 transform transition">
             <img src="/images/Logo-webpage.png" alt="Logo website" />
-          </a>
+          </Link>
           {categories.map((category, id) => (
-            <Navitem key={id} name={category} />
+            <Navitem key={id} category={category} />
           ))}
           <CartWidget />
         </ul>
--- src/components/Navbar/Navitem.jsx
+++ src/components/Navbar/Navitem.jsx
@@ -1,9 +1,13 @@
-const Navitem = (props) => {
-    return (
-      <div>
-        <a className="px-4 py-2.5 text-sm font-semibold bg-transparent rounded-full ml-1 focus:text-white hover:text-white hover:bg-violet-600 focus:bg-violet-600 focus:outline-none focus:shadow-outline" href="#"> {props.name} </a>
-      </div>
-    );
-  }
-  
-  export default Navitem;
+import { Link, NavLink } from "react-router-dom";
+
+const Navitem = ({ category }) => {
+  return (
+      <Link
+        className="px-4 py-2.5 text-sm font-semibold bg-transparent rounded-full ml-1 focus:text-white hover:text-white hover:bg-violet-600 focus:bg-violet-600 focus:outline-none focus:shadow-outline"
+        to={`/category/${category}`}>
+        {category}
+      </Link>
+  );
+}
+
+export default Navitem;
