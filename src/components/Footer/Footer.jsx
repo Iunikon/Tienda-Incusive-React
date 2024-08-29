d9f37c616c4ea380f40b329a62189f9bcdbc70ad
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-29 13:41:02 -0600

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,8 @@
 const Footer = () => {
     return (
       // Footer container
-      <footer className="mt-12 pt-10 flex flex-col items-center bg-violet-700 text-center text-white">
-        <div className="w-full bg-black/20 p-4 text-center">
+      <footer className="mt-20  flex flex-col items-center bg-violet-700 text-center text-white">
+        <div className="w-full p-4 text-center">
         <span className="text-xs">
         <strong>Skims: </strong>Ropa Interior Inclusiva 2023
         </span>
