7fb2a9a963f00cbd41ac28d5390a4cb23ed5cef8
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-29 13:41:13 -0600

    Add 404

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,28 @@ import Layout from './page/Layout.jsx';
 import Main from './components/Main/Main.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import AboutUs from './components/AboutUs/AbouUs.jsx';
+import Error404 from './components/Error404/Error404.jsx';
 
 function App() {
-  const title = "Titulo ejemplo";
+const title = "Titulo ejemplo";
 
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/"  element={<>
-                <Main/>
-                <ItemListContainer title={title} />
-              </>
-            } 
-          />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<AboutUs />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+return (
+<BrowserRouter>
+  <Layout>
+    <Routes>
+      <Route path="/" element={<>
+        <Main />
+        <ItemListContainer title={title} />
+        </>
+        }
+        />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="*" element={<Error404 />} />
+    </Routes>
+  </Layout>
+</BrowserRouter>
+);
 }
 
 export default App;
