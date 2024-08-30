a23239cb4912b4611a8c7cc6c1f6c1e67f68c682
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-30 15:18:42 -0600

    Cambios hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,22 @@
-function Hero () {
-return (
-
-<div>
-    <div className="relative z-20 mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-3xl text-center">
-            <h1 className="font-bold tracking-tight text-slate-800 text-6xl">
-                Inclusive
-                <span className="text-violet-600"> underwear</span>
-            </h1>
-            <p className="mt-4 text-lg leading-8 text-slate-600">
-                Ropa interior inclusiva, diseñada para celebrar y respetar la diversidad de cuerpos y géneros,
-                ofreciendo comodidad y estilo para todas las identidades.
-            </p>
-            <div className="mt-6 flex items-center justify-center gap-x-6">
+const Hero = () => {
+    return (
+        <div>
+            <div className="relative z-20 mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-3xl text-center">
+                    <h1 className="font-bold tracking-tight text-slate-800 text-6xl">
+                        Inclusive
+                        <span className="text-violet-600"> underwear</span>
+                    </h1>
+                    <p className="mt-4 text-lg leading-8 text-slate-600">
+                        Ropa interior inclusiva, diseñada para celebrar y respetar la diversidad de cuerpos y géneros,
+                        ofreciendo comodidad y estilo para todas las identidades.
+                    </p>
+                    <div className="mt-6 flex items-center justify-center gap-x-6">
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-);
+    );
 }
 
 export default Hero;
