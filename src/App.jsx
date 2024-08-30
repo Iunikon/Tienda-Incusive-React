28494cfc6ef77f4be33f9c439b0f531ff5603c83
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-30 15:17:12 -0600

    Item detail

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Layout from './page/Layout.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import AboutUs from './components/AboutUs/AbouUs.jsx';
 import Error404 from './components/Error404/Error404.jsx';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 
 function App() {
 const title = "Titulo ejemplo";
@@ -19,6 +20,7 @@ return (
         }
         />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
+        <Route path="/product/:id" element={<ItemDetailContainer />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="*" element={<Error404 />} />
---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProductById } from "../../utils/fetchData";
+import Spinner from "../Spinner/Spinner";
+import ItemDetail from "../ItemDetail/ItemDetail";
+
+
+const ItemDetailContainer = () => {
+    const [product, setProduct] = useState({});
+    const [loading, setLoading] = useState(true);
+    const { id } = useParams();
+
+    useEffect(() => {
+        setLoading(true);
+        getProductById(id) 
+            .then((res) => {
+                setProduct(res);
+            })
+            .catch((err) => {
+                console.error("Error fetching product:", err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, [id]);
+
+    return (
+        <main>
+            {loading ? (
+                <>
+                    <Spinner />
+                </>
+            ) : (
+                <div>
+                    <ItemDetail product={product} />
+                </div>
+            )}
+        </main>
+    );
+};
+
+export default ItemDetailContainer;
+
+
