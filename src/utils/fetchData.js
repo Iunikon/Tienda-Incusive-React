0b7124d28fa3525d9e2ef9ac947c60013013f82d
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-29 19:37:32 -0600

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,15 +1,21 @@
 import { items } from "../mock/mockData";
 
 export const getProducts = (category) => {
-    const filteredItems = items.filter((item) => 
-        item.category.includes (category)
-    );
+
+    let filteredItems
+    if (category) {
+        filteredItems = items.filter((item) => 
+            item.category.includes (category)
+        );
+    }
+    else { filteredItems  = items}
+
 
     return new Promise((resolve, reject) => {
         if (filteredItems.length > 0) {
             setTimeout(() => {
                 resolve(filteredItems);
-            }, 2000);
+            }, 500);
         } else {
             reject("No hay productos disponibles");
         }
