7a4c3cd57a551d38c95e4d7c3a69a370ab40b338
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-29 19:38:02 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
 import Layout from './page/Layout.jsx';
-import Main from './components/Main/Main.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import AboutUs from './components/AboutUs/AbouUs.jsx';
 import Error404 from './components/Error404/Error404.jsx';
@@ -15,11 +14,11 @@ return (
   <Layout>
     <Routes>
       <Route path="/" element={<>
-        <Main />
         <ItemListContainer title={title} />
         </>
         }
         />
+        <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="*" element={<Error404 />} />
