1184573a7b65f5adb32434cb00aad22760f7aba0
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-29 19:37:01 -0600

    Cambiar main por hero

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,12 +2,11 @@ import ItemCount from "../Item/ItemCount";
 
 const Item = ({ product }) => {
   return (
-    <div className="p-4 border rounded shadow-sm">
+    <div className="p-4 border rounded shadow-sm  bg-white">
       <img src={product.imageUrl} alt={product.name} className="w-full h-auto object-cover" />
-      <h3 className="text-slate-700 text-lg font-bold">{product.name}</h3>
-      <p className="text-slate-600 text-md">{product.description}</p>
-      <p className="text-slate-500 text-sm">Price: ${product.price}</p>
-      <ItemCount stock={10} initial={1} />
+      <h3 className="text-slate-600 text-lg font-bold">{product.name}</h3>
+      <p className="text-slate-500 mb-2 text-md">${product.price}</p>
+      <p className="text-slate-500 text-sm">{product.description}</p>      <ItemCount stock={10} initial={1} />
     </div>
   );
 };
