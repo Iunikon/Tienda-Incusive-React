390de7be38d1d16d1f79bef5d6534deffbeca3d9
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-30 22:11:33 -0600

    Update Item.jsx

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,19 @@
+import { useCartContext } from "../../context/CartContext";
 import ItemCount from "../Item/ItemCount";
 import { Link } from "react-router-dom";
 
 const Item = ({ product }) => {
+  const { addToCart } = useCartContext();
+
+  const handleOnBuy = (qty) => {
+    console.log(`Se agregaron ${qty} productos al carrito`);
+    if (typeof addToCart === 'function') {
+      addToCart(product, qty);
+    } else {
+      console.error('addToCart no es una función');
+    }
+  };
+
   return (
     <div className="relative flex flex-col h-full p-4 border rounded-lg shadow-sm bg-white overflow-hidden">
       <div className="relative">
@@ -11,7 +23,7 @@ const Item = ({ product }) => {
           className="w-full rounded-lg h-auto object-cover"
         />
         <div className="absolute inset-0 bg-white bg-opacity-5 backdrop-blur-sm rounded-lg flex items-center justify-center opacity-0 hover:opacity-100 transition-opacity duration-200">
-        <Link to={`/product/${product.id}`}>
+          <Link to={`/product/${product.id}`}>
             <button className="px-4 py-2 bg-white text-slate-600 rounded-full transition-colors duration-300 hover:text-white hover:bg-violet-600">
               Ver detalles
             </button>
@@ -23,7 +35,7 @@ const Item = ({ product }) => {
       <p className="text-slate-500 text-sm mb-4">{product.description}</p>
       
       <div className="items-center mt-auto">
-        <ItemCount stock={10} initial={1} />
+        <ItemCount stock={product.stock} initial={1} handleOnBuy={handleOnBuy} />
       </div>
     </div>
   );
