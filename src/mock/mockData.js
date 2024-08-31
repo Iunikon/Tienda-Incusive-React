1697bf360c5ea3ebb333a3887ba80ee9aa03f17b
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-31 13:08:45 -0600

    Update mockData.js

--- src/mock/mockData.js
+++ src/mock/mockData.js
@@ -3,7 +3,6 @@ export const categories = ["Deportiva", "Casual", "Erótica", "Descanso"];
 
 export const items = [
     {
-        id: 1,
         name: "boxer",
         description: "Cómodos bóxers de algodón azul perfectos para el uso diario.",
         price: 100,
@@ -12,7 +11,6 @@ export const items = [
         image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
     },
     {
-        id: 2,
         name: "bralette",
         description: "Bralette de encaje suave que ofrece soporte y estilo, ideal para el descanso.",
         price: 150,
@@ -21,7 +19,6 @@ export const items = [
         image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
     },
     {
-        id: 3,
         name: "tanga",
         description: "Tanga de microfibra transpirable diseñada para un ajuste cómodo durante el ejercicio.",
         price: 120,
@@ -30,7 +27,6 @@ export const items = [
         image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
     },
     {
-        id: 4,
         name: "jockstrap",
         description: "Jockstrap de diseño ergonómico, ideal para deportes de alto rendimiento.",
         price: 130,
@@ -39,7 +35,6 @@ export const items = [
         image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
     },
     {
-        id: 5,
         name: "lencería",
         description: "Conjunto de lencería erótica con detalles en encaje, perfecto para ocasiones especiales.",
         price: 200,
@@ -48,7 +43,6 @@ export const items = [
         image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
     },
     {
-        id: 6,
         name: "bikini",
         description: "Bikini de algodón orgánico para uso casual, disponible en varios colores.",
         price: 110,
