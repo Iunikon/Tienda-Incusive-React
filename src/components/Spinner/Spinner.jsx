748a852bcb0a765e1aab13b9b10d166ea2ba30ea
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-30 18:27:35 -0600

    Update Spinner.jsx

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,6 +1,6 @@
 const Spinner = () => {
 return (
-<div className="flex items-center justify-center  min-h-[60vh]" aria-label="Loading...">
+<div className="flex items-center justify-center min-h-[60vh]" aria-label="Loading...">
     <div
         className="w-12 h-12 border-4 border-t-4 border-gray-200 border-t-violet-600 border-solid rounded-full animate-spin">
     </div>
