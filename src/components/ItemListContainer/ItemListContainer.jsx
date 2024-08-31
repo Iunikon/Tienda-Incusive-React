0cd668e99cc62cbbc4ff244023fc833d6c6476c3
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-30 18:24:19 -0600

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useCartContext } from "../../context/CartContext";
 import ItemList from "../Itemlist/ItemList";
 import { getProducts } from "../../utils/fetchData";
 import { useParams } from "react-router-dom";
@@ -7,10 +8,14 @@ import Hero from "../Hero/Hero";
 
 
 
-const ItemListContainer = ({ title }) => {
+const ItemListContainer = () => {
 const [products, setProducts] = useState([]);
 const { categoryId } = useParams();
 const [loading, setLoading] = useState(true);
+const { titulo } = useCartContext ();
+
+let titleToShow = titulo;
+
 
 
 useEffect(() => {
@@ -41,8 +46,8 @@ return (
     <Spinner /> 
     ) : (
     <div>
-        <h2 className="text-3xl font-bold text-slate-700 mb-1">{title}</h2>
-        <ItemList products={products} title={title} />
+        <h2 className="text-3xl font-bold text-slate-700 mb-1">{titleToShow}</h2>
+        <ItemList products={products} title={titleToShow} />
     </div>
     )}
 </main>
