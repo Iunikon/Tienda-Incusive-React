e3653a47445d044a858ac9afb240f24d397f56c0
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-31 13:09:25 -0600

    Update ItemListContainer.jsx

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,60 +1,60 @@
 import React, { useEffect, useState } from "react";
+import { db } from "../../firebase/dbConnection";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useCartContext } from "../../context/CartContext";
 import ItemList from "../Itemlist/ItemList";
-import { getProducts } from "../../utils/fetchData";
 import { useParams } from "react-router-dom";
 import Spinner from "../Spinner/Spinner";
 import Hero from "../Hero/Hero";
 
-
-
 const ItemListContainer = () => {
-const [products, setProducts] = useState([]);
-const { categoryId } = useParams();
-const [loading, setLoading] = useState(true);
-const {titulo } = useCartContext ();
+    const [products, setProducts] = useState([]);
+    const { categoryId } = useParams();
+    const [loading, setLoading] = useState(true);
+    const { titulo } = useCartContext();
 
-let titleToShow = titulo;
+    let titleToShow = titulo;
 
+        
+    useEffect(() => {
+        setLoading(true);
+        const productsCollection = collection(db, "productos");
 
+        let ProductsCollectionFiltered;
+        if (categoryId) {
+            ProductsCollectionFiltered = query(productsCollection, where("category", "array-contains", categoryId));
+        } else {
+            ProductsCollectionFiltered = productsCollection;
+        }
 
+        getDocs(ProductsCollectionFiltered)
+            .then((querySnapshot) => {
+                const prodFromDocs = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    ...doc.data(),
+                }));
+                setProducts(prodFromDocs);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error("Error getting documents:", error);
+                setLoading(false);
+            });
+    }, [categoryId]);
 
-
-useEffect(() => {
-console.log("Se montó el componente");
-setLoading(true);
-getProducts(categoryId)
-.then((res) => {
-console.log("Se ejecutó la promesa");
-setProducts(res);
-console.log("Se actualizaron los productos");
-})
-.catch((err) => {
-console.error("Error al obtener los productos:", err);
-})
-.finally(() => {
-console.log("Finalizó la promesa");
-setLoading(false);
-});
-}, [categoryId]);
-
-
-
-return (
-<main>
-<Hero />
-    {console.log("Renderizo el componente")}
-    {loading ? (
-    <Spinner /> 
-    ) : (
-    <div>
-        <h2 className="text-3xl font-bold text-slate-700 mb-1">{titleToShow}</h2>
-        <ItemList products={products} title={titleToShow} />
-    </div>
-    )}
-</main>
-);
-
+    return (
+        <main>
+            <Hero />
+            {loading ? (
+                <Spinner />
+            ) : (
+                <div>
+                    <h2 className="text-3xl font-bold text-slate-700 mb-1">{titleToShow}</h2>
+                    <ItemList products={products} title={titleToShow} />
+                </div>
+            )}
+        </main>
+    );
 };
 
 export default ItemListContainer;
