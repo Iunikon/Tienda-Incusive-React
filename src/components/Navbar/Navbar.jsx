40f27665d3f1979392c3bc1bbc522a36c28ac4ab
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-30 23:00:49 -0600

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
   const handleGoToCheckout = () => {
     navigate("/cart");
   };
+  
 
   return (
     <nav className="fixed top-2 left-0 right-0 bg-slate-100 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-70 border border-slate-200 z-50 mx-auto text-slate-600 p-4 rounded-full max-w-3xl shadow-sm">
