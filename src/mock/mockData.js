a51a4712b763a3599a999f8383c2bfea892f652e
Jorge Miguel Pérez Roblez <jorgemiguelperezroblez@gmail.com>
2024-08-31 20:33:08 -0600

    Update mockData.js

--- src/mock/mockData.js
+++ src/mock/mockData.js
@@ -2,52 +2,135 @@ export const categories = ["Deportiva", "Casual", "Erótica", "Descanso"];
 
 
 export const items = [
-    {
-        name: "boxer",
-        description: "Cómodos bóxers de algodón azul perfectos para el uso diario.",
-        price: 100,
-        stock: 10,
-        category: ["Casual"],
-        image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
-    },
-    {
-        name: "bralette",
-        description: "Bralette de encaje suave que ofrece soporte y estilo, ideal para el descanso.",
-        price: 150,
-        stock: 15,
-        category: ["Descanso"],
-        image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
-    },
-    {
-        name: "tanga",
-        description: "Tanga de microfibra transpirable diseñada para un ajuste cómodo durante el ejercicio.",
-        price: 120,
-        stock: 20,
-        category: ["Erótica"],
-        image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
-    },
-    {
-        name: "jockstrap",
-        description: "Jockstrap de diseño ergonómico, ideal para deportes de alto rendimiento.",
-        price: 130,
-        stock: 25,
-        category: ["Deportiva"],
-        image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
-    },
-    {
-        name: "lencería",
-        description: "Conjunto de lencería erótica con detalles en encaje, perfecto para ocasiones especiales.",
-        price: 200,
-        stock: 5,
-        category: ["Erótica"],
-        image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
-    },
-    {
-        name: "bikini",
-        description: "Bikini de algodón orgánico para uso casual, disponible en varios colores.",
-        price: 110,
-        stock: 30,
-        category: ["Casual"],
-        image: "https://res.cloudinary.com/dvfa86pha/image/upload/v1725045900/cld-sample-5.jpg"
-    }
+    [
+        {
+            "name": "Jockstrap Deportivo",
+            "description": "Jockstrap de malla transpirable con soporte adicional, ideal para actividades físicas intensas.",
+            "price": 130,
+            "stock": 15,
+            "category": ["Deportiva"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157357/Jockstrap_Deportivo_jpjqw1.jpg"
+        },
+        {
+            "name": "Sujetador Deportivo",
+            "description": "Sujetador sin costuras que ofrece máximo soporte y comodidad durante entrenamientos.",
+            "price": 150,
+            "stock": 20,
+            "category": ["Deportiva"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157360/Sujetador_Deportivo_fe3jrs.jpg"
+        },
+        {
+            "name": "Leggings Compresivos",
+            "description": "Leggings de alta compresión, diseñados para mejorar el rendimiento deportivo.",
+            "price": 200,
+            "stock": 25,
+            "category": ["Deportiva"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157357/Leggings_Compresivos_giypqp.jpg"
+        },
+        {
+            "name": "Bóxer Deportivo",
+            "description": "Bóxer de ajuste perfecto, fabricado en material de secado rápido para entrenamientos intensos.",
+            "price": 120,
+            "stock": 18,
+            "category": ["Deportiva"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157354/B%C3%B3xer_Deportivo_nztpjs.jpg"
+        },
+        {
+            "name": "Bikini de Algodón",
+            "description": "Bikini de algodón suave, perfecto para el uso diario con diseño sencillo y elegante.",
+            "price": 110,
+            "stock": 30,
+            "category": ["Casual"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157353/Bikini_de_Algod%C3%B3n_jxrwtx.jpg"
+        },
+        {
+            "name": "Bóxer Clásico",
+            "description": "Bóxer clásico de corte medio, ideal para el día a día con estilo atemporal.",
+            "price": 100,
+            "stock": 20,
+            "category": ["Casual"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157354/B%C3%B3xer_Cl%C3%A1sico_g9bcmk.jpg"
+        },
+        {
+            "name": "Braguita de Encaje",
+            "description": "Braguita de encaje floral, que combina comodidad y feminidad para un look casual.",
+            "price": 90,
+            "stock": 15,
+            "category": ["Casual"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157354/Braguita_de_Encaje_hzotnk.jpg"
+        },
+        {
+            "name": "Tanga de Algodón",
+            "description": "Tanga suave y cómoda, hecha de algodón orgánico para un ajuste perfecto en cualquier ocasión.",
+            "price": 85,
+            "stock": 25,
+            "category": ["Casual"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157361/Tanga_de_Algod%C3%B3n_yx6eej.jpg"
+        },
+        {
+            "name": "Lencería de Seda",
+            "description": "Conjunto de lencería de seda con detalles en encaje, ideal para momentos íntimos.",
+            "price": 250,
+            "stock": 10,
+            "category": ["Erótica"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157358/Lencer%C3%ADa_de_Seda_cih5nv.jpg"
+        },
+        {
+            "name": "Bodysuit Transparente",
+            "description": "Bodysuit de encaje con transparencias, diseñado para realzar la figura.",
+            "price": 220,
+            "stock": 12,
+            "category": ["Erótica"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157353/Bodysuit_Transparente_avyugd.jpg"
+        },
+        {
+            "name": "Corset de Encaje",
+            "description": "Corset ajustado con detalles de encaje, perfecto para una noche especial.",
+            "price": 270,
+            "stock": 8,
+            "category": ["Erótica"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157356/Corset_de_Encaje_m4kkjl.jpg"
+        },
+        {
+            "name": "Medias de Red",
+            "description": "Medias de red con liguero integrado, para un look seductor y elegante.",
+            "price": 90,
+            "stock": 30,
+            "category": ["Erótica"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157359/Medias_de_red_dopftg.jpg"
+        },
+        {
+            "name": "Pijama de Algodón",
+            "description": "Pijama de dos piezas hecho de algodón suave, perfecto para una noche de descanso.",
+            "price": 180,
+            "stock": 15,
+            "category": ["Descanso"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157360/Pijama_de_Algod%C3%B3n_f5jvnw.jpg"
+        },
+        {
+            "name": "Camisón de Seda",
+            "description": "Camisón de seda con diseño relajado, ideal para dormir con lujo y confort.",
+            "price": 220,
+            "stock": 10,
+            "category": ["Descanso"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157355/Camis%C3%B3n_de_Seda_kd7hfm.jpg"
+        },
+        {
+            "name": "Bata de Algodón",
+            "description": "Bata larga de algodón, perfecta para relajarse en casa después de un largo día.",
+            "price": 140,
+            "stock": 20,
+            "category": ["Descanso"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157761/Bata_algodon_gmqjtp.jpg"
+        },
+        {
+            "name": "Boxer de Dormir",
+            "description": "Boxer ligero y suelto para dormir, hecho de tejido transpirable.",
+            "price": 110,
+            "stock": 25,
+            "category": ["Descanso"],
+            "image": "https://res.cloudinary.com/dvfa86pha/image/upload/v1725157825/boxer-dormir_qqf2er.jpg"
+        }
+    ]
+    
 ];
